849f0480e7910dc33a67b113a9b5ace61624f428
Joel Noyce Barnham <joelnbarnham@gmail.com>
2019-01-08 08:11:38 +0000

    Return the error if we got one
    If not then return an error indicating that this is not yet fully
    implemented.

--- cmd/xenstore/transport_win.go
+++ cmd/xenstore/transport_win.go
@@ -3,11 +3,17 @@
 package main
 
 import (
+	"errors"
+
 	"github.com/joelnb/xenstore-go"
 	"github.com/urfave/cli"
 )
 
 func getTransport(ctx *cli.Context) (xenstore.Transport, error) {
 	err := xenstore.NewWinPVTransport()
-	return nil, err
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, errors.New("Not yet implemented")
 }
