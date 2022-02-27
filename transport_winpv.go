011762bd70d26649693adf9428c778d5ecb8cde4
Joel Noyce Barnham <joelnbarnham@gmail.com>
2022-02-27 20:59:17 +0000

    Implement more of WMI API

--- transport_winpv.go
+++ transport_winpv.go
@@ -5,15 +5,20 @@ package xenstore
 
 import (
 	"fmt"
+	"strings"
 
+	"github.com/go-ole/go-ole"
 	log "github.com/sirupsen/logrus"
 	"github.com/yusufpapurcu/wmi"
 )
 
+const SessionName = "xenstore-go"
+
 type WinPVTransport struct {
 	base       *XenProjectXenStoreBase
-	session    *XenProjectXenStoreSession
 	pktChannel chan *Packet
+
+	Session *XenProjectXenStoreSession
 }
 
 func (t *WinPVTransport) Close() error {
@@ -23,17 +28,21 @@ func (t *WinPVTransport) Close() error {
 		t.base.disp.Release()
 	}
 
-	if t.session != nil {
-		if err := t.session.EndSession(); err != nil {
-			return err
-		}
-	}
-
 	return nil
 }
 
 func (t *WinPVTransport) Send(pkt *Packet) error {
-	packet := &Packet{
+	var err error
+
+	session := t.Session
+	if t.Session == nil {
+		session, err = t.base.AddSession(SessionName)
+		if err != nil {
+			return err
+		}
+	}
+
+	rpkt := &Packet{
 		Header: &PacketHeader{
 			Op:   pkt.Header.Op,
 			RqId: pkt.Header.RqId,
@@ -41,16 +50,43 @@ func (t *WinPVTransport) Send(pkt *Packet) error {
 		},
 	}
 
-	if pkt.Header.Op == XsRead {
-		val, err := t.session.GetValue(string(pkt.Payload))
+	switch pkt.Header.Op {
+	case XsDirectory:
+		val, err := session.GetChildren(string(pkt.Payload))
 		if err != nil {
 			return err
 		}
-		packet.Header.Length = uint32(len(val))
-		packet.Payload = []byte(val)
+
+		rpkt.Payload = []byte(strings.Join(val, "\000"))
+	case XsRead:
+		val, err := session.GetValue(string(pkt.Payload))
+		if err != nil {
+			return err
+		}
+
+		rpkt.Payload = []byte(val)
+	case XsRm:
+		if err := session.RemoveValue(string(pkt.Payload)); err != nil {
+			return err
+		}
+	case XsWrite:
+		args := pkt.Strings()
+		if err := session.SetValue(args[0], args[1]); err != nil {
+			return err
+		}
+	default:
+		return fmt.Errorf("WinPVTransport: Unsupported packet: %+v", pkt)
 	}
 
-	t.pktChannel <- packet
+	rpkt.Header.Length = uint32(len(rpkt.Payload))
+
+	t.pktChannel <- rpkt
+
+	if t.Session == nil {
+		if err := session.EndSession(); err != nil {
+			return err
+		}
+	}
 
 	return nil
 }
@@ -105,15 +141,7 @@ func (t *WinPVTransport) GetBase() (*XenProjectXenStoreBase, error) {
 }
 
 func (t *WinPVTransport) GetSession(sessionId int32) (*XenProjectXenStoreSession, error) {
-	if t.session != nil {
-		return t.session, nil
-	}
-
-	log.Debug("GetSession: Requesting session list")
-
-	var sessionList []XenProjectXenStoreSessionProps
-	sessionQuery := fmt.Sprintf("SELECT Active, ID, InstanceName, SessionID FROM XenProjectXenStoreSession WHERE SessionId=%d", sessionId)
-	sessionDispatchList, err := wmi.QueryNamespaceRaw(sessionQuery, &sessionList, "root\\wmi")
+	sessionList, sessionDispatchList, err := t.getSessions(sessionId)
 	if err != nil {
 		return nil, err
 	}
@@ -124,14 +152,7 @@ func (t *WinPVTransport) GetSession(sessionId int32) (*XenProjectXenStoreSession
 		}
 	}
 
-	log.Debugf("GetSession: Got session list: %+v", sessionList)
-
-	if len(sessionList) != len(sessionDispatchList) {
-		cleanup()
-		return nil, fmt.Errorf("WinPVTransport.GetSession: unexpected length mismatch (sessionList=%d, sessionDispatchList=%d)",
-			len(sessionList),
-			len(sessionDispatchList))
-	} else if len(sessionList) == 0 {
+	if len(sessionList) == 0 {
 		cleanup()
 		return nil, fmt.Errorf("WinPVTransport.GetSession: Got 0-length list")
 	} else if len(sessionList) > 1 {
@@ -139,13 +160,59 @@ func (t *WinPVTransport) GetSession(sessionId int32) (*XenProjectXenStoreSession
 		return nil, fmt.Errorf("WinPVTransport.GetSession: Unexpected multiple XenProjectXenStoreBase returned")
 	}
 
-	t.session = &XenProjectXenStoreSession{
+	return &XenProjectXenStoreSession{
 		transport:  t,
 		disp:       sessionDispatchList[0],
 		Properties: sessionList[0],
+	}, nil
+}
+
+func (t *WinPVTransport) GetSessions() ([]*XenProjectXenStoreSession, error) {
+	var sessions []*XenProjectXenStoreSession
+
+	sessionList, sessionDispatchList, err := t.getSessions()
+	if err != nil {
+		return nil, err
 	}
 
-	return t.session, nil
+	for i := range sessionList {
+		sessions = append(sessions, &XenProjectXenStoreSession{
+			transport:  t,
+			disp:       sessionDispatchList[i],
+			Properties: sessionList[i],
+		})
+	}
+
+	return sessions, nil
+}
+
+func (t *WinPVTransport) getSessions(sessionIds ...int32) ([]XenProjectXenStoreSessionProps, []*ole.IDispatch, error) {
+	log.Debug("WinPVTransport.getSessions: Requesting session list")
+
+	sessionQuery := "SELECT Active, ID, InstanceName, SessionID FROM XenProjectXenStoreSession"
+	if len(sessionIds) > 0 {
+		sessionQuery = fmt.Sprintf("%s WHERE SessionId=%d", sessionQuery, sessionIds[0])
+	}
+
+	var sessionList []XenProjectXenStoreSessionProps
+	sessionDispatchList, err := wmi.QueryNamespaceRaw(sessionQuery, &sessionList, "root\\wmi")
+	if err != nil {
+		return []XenProjectXenStoreSessionProps{}, []*ole.IDispatch{}, err
+	}
+
+	log.Debugf("WinPVTransport.getSessions: Got session list: %+v", sessionList)
+
+	if len(sessionList) != len(sessionDispatchList) {
+		for _, item := range sessionDispatchList {
+			item.Release()
+		}
+
+		return []XenProjectXenStoreSessionProps{}, []*ole.IDispatch{}, fmt.Errorf("WinPVTransport.getSessions: unexpected length mismatch (sessionList=%d, sessionDispatchList=%d)",
+			len(sessionList),
+			len(sessionDispatchList))
+	}
+
+	return sessionList, sessionDispatchList, nil
 }
 
 func NewWinPVTransport() (*WinPVTransport, error) {
@@ -158,30 +225,10 @@ func NewWinPVTransport() (*WinPVTransport, error) {
 	c := make(chan *Packet)
 	transport.pktChannel = c
 
-	base, err := transport.GetBase()
+	_, err := transport.GetBase()
 	if err != nil {
 		return nil, err
 	}
-
-	session, err := base.AddSession("JoelSession")
-	if err != nil {
-		return nil, err
-	}
-	transport.session = session
-
-	value, err := session.GetValue("vm")
-	if err != nil {
-		return nil, err
-	}
-
-	fmt.Println(value)
-
-	children, err := session.GetChildren(value)
-	if err != nil {
-		return nil, err
-	}
-
-	fmt.Printf("%+v\n", children)
 
 	return transport, nil
 }
--- winpv_wmi.go
+++ winpv_wmi.go
@@ -1,6 +1,26 @@
 //go:build windows
 // +build windows
 
+/*
+Some examples of how to use the PowerShell API
+
+```
+# Remove all sessions & confirm they are gone
+$sessions = Get-WmiObject -Namespace root\wmi -Class XenProjectXenStoreSession
+$sessions | ForEach-Object { $_.EndSession() }
+Get-WmiObject -Namespace root\wmi -Class XenProjectXenStoreSession
+
+# Get a value & list a directory
+$base = Get-WmiObject -Namespace root\wmi -Class XenProjectXenStoreBase -ErrorAction SilentlyContinue
+$sid = $base.AddSession("ExampleSession")
+$session = Get-WmiObject -Namespace root\wmi -Query "select * from XenProjectXenStoreSession where SessionId=$($sid.SessionId)"
+$my_path = $session.GetValue("vm")['value']
+$my_nodes = $session.GetChildren($my_path)
+$my_nodes.children.ChildNodes
+$session.EndSession()
+```
+*/
+
 package xenstore
 
 import (
@@ -22,8 +42,9 @@ func initWmi() error {
 }
 
 type XenProjectXenStoreBase struct {
-	transport  *WinPVTransport
-	disp       *ole.IDispatch
+	transport *WinPVTransport
+	disp      *ole.IDispatch
+
 	Properties XenProjectXenStoreBaseProps
 }
 
@@ -34,8 +55,9 @@ type XenProjectXenStoreBaseProps struct {
 }
 
 type XenProjectXenStoreSession struct {
-	transport  *WinPVTransport
-	disp       *ole.IDispatch
+	transport *WinPVTransport
+	disp      *ole.IDispatch
+
 	Properties XenProjectXenStoreSessionProps
 }
 
@@ -55,11 +77,24 @@ type XenProjectXenStoreUnsuspendedEvent struct {
 	SessionID uint32
 }
 
+func resultVariant() *ole.VARIANT {
+	variant := new(ole.VARIANT)
+	ole.VariantInit(variant)
+	return variant
+}
+
+func (s *XenProjectXenStoreSession) callMethod(name string, params ...interface{}) (*ole.VARIANT, error) {
+	resultRaw, err := oleutil.CallMethod(s.disp, name, params...)
+	if err != nil {
+		return nil, fmt.Errorf("CallMethod XenProjectXenStoreSession.%s: %v", name, err)
+	}
+	return resultRaw, nil
+}
+
 func (b *XenProjectXenStoreBase) AddSession(name string) (*XenProjectXenStoreSession, error) {
 	log.Debug("AddSession: Adding session")
 
-	sessionResultRaw := new(ole.VARIANT)
-	ole.VariantInit(sessionResultRaw)
+	sessionResultRaw := resultVariant()
 	defer sessionResultRaw.Clear()
 
 	methodName := "AddSession"
@@ -71,41 +106,58 @@ func (b *XenProjectXenStoreBase) AddSession(name string) (*XenProjectXenStoreSes
 
 	log.Debugf("AddSession: Added session: %+v", sessionResultRaw)
 
-	sessionId := sessionResultRaw.Value().(int32)
-
-	return b.transport.GetSession(sessionId)
+	return b.transport.GetSession(sessionResultRaw.Value().(int32))
 }
 
 func (s *XenProjectXenStoreSession) GetValue(path string) (string, error) {
 	log.Debugf("GetValue: Getting value for path: %s", path)
 
-	valueResultRaw := new(ole.VARIANT)
-	ole.VariantInit(valueResultRaw)
+	valueResultRaw := resultVariant()
 	defer valueResultRaw.Clear()
 
-	methodName := "GetValue"
-	resultRaw, err := oleutil.CallMethod(s.disp, methodName, path, valueResultRaw)
+	resultRaw, err := s.callMethod("GetValue", path, valueResultRaw)
 	if err != nil {
-		return "", fmt.Errorf("CallMethod XenProjectXenStoreBase.%s: %v", methodName, err)
+		return "", err
 	}
 	defer resultRaw.Clear()
 
 	return valueResultRaw.ToString(), nil
 }
 
+func (s *XenProjectXenStoreSession) SetValue(path, value string) error {
+	log.Debugf("SetValue: Setting value '%s' for path: %s", value, path)
+
+	resultRaw, err := s.callMethod("SetValue", path, value)
+	if err != nil {
+		return err
+	}
+	defer resultRaw.Clear()
+
+	return nil
+}
+
+func (s *XenProjectXenStoreSession) Log(msg string) error {
+	resultRaw, err := s.callMethod("Log", msg)
+	if err != nil {
+		return err
+	}
+	defer resultRaw.Clear()
+
+	return nil
+}
+
+// GetChildren lists the children of the requested path.
 func (s *XenProjectXenStoreSession) GetChildren(path string) ([]string, error) {
 	log.Debugf("GetChildren: Getting children for path: %s", path)
 
 	var stringNodes []string
 
-	valueResultRaw := new(ole.VARIANT)
-	ole.VariantInit(valueResultRaw)
+	valueResultRaw := resultVariant()
 	defer valueResultRaw.Clear()
 
-	methodName := "GetChildren"
-	resultRaw, err := oleutil.CallMethod(s.disp, methodName, path, valueResultRaw)
+	resultRaw, err := s.callMethod("GetChildren", path, valueResultRaw)
 	if err != nil {
-		return stringNodes, fmt.Errorf("CallMethod XenProjectXenStoreBase.%s: %v", methodName, err)
+		return nil, err
 	}
 	defer resultRaw.Clear()
 
@@ -131,12 +183,75 @@ func (s *XenProjectXenStoreSession) EndSession() error {
 
 	defer s.disp.Release()
 
-	methodName := "EndSession"
-	resultRaw, err := oleutil.CallMethod(s.disp, methodName)
-	if err != nil {
-		return fmt.Errorf("CallMethod XenProjectXenStoreBase.%s: %v", methodName, err)
-	}
+	resultRaw, err := s.callMethod("EndSession")
 	defer resultRaw.Clear()
 
-	return nil
+	return err
+}
+
+// RemoveValue removes a Xenstore key.
+func (s *XenProjectXenStoreSession) RemoveValue(path string) error {
+	log.Debugf("RemoveValue: Removing value: %s", path)
+
+	defer s.disp.Release()
+
+	resultRaw, err := s.callMethod("RemoveValue", path)
+	defer resultRaw.Clear()
+
+	return err
+}
+
+// SetWatch creates a Xenstore watch.
+// TODO: Allow subscribing to the WMI events related to the watch.
+func (s *XenProjectXenStoreSession) SetWatch(path string) error {
+	log.Debugf("SetWatch: Setting watch on: %s", path)
+
+	defer s.disp.Release()
+
+	resultRaw, err := s.callMethod("SetWatch", path)
+	defer resultRaw.Clear()
+
+	return err
+}
+
+// RemoveWatch removes a previously set Xenstore watch.
+func (s *XenProjectXenStoreSession) RemoveWatch(path string) error {
+	log.Debugf("RemoveWatch: Removing watch on: %s", path)
+
+	defer s.disp.Release()
+
+	resultRaw, err := s.callMethod("RemoveWatch", path)
+	defer resultRaw.Clear()
+
+	return err
+}
+
+// StartTransaction starts a new transaction to atomically group some actions.
+func (s *XenProjectXenStoreSession) StartTransaction() error {
+	defer s.disp.Release()
+
+	resultRaw, err := s.callMethod("StartTransaction")
+	defer resultRaw.Clear()
+
+	return err
+}
+
+// CommitTransaction ends the transaction, causing all of the actions within it to be processed.
+func (s *XenProjectXenStoreSession) CommitTransaction() error {
+	defer s.disp.Release()
+
+	resultRaw, err := s.callMethod("CommitTransaction")
+	defer resultRaw.Clear()
+
+	return err
+}
+
+// AbortTransaction aborts a transaction, discarding any actions that were done since it was started.
+func (s *XenProjectXenStoreSession) AbortTransaction() error {
+	defer s.disp.Release()
+
+	resultRaw, err := s.callMethod("AbortTransaction")
+	defer resultRaw.Clear()
+
+	return err
 }
