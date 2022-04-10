fc55176baf1b7e2aafe644282c0c19757dfcb746
Joel Noyce Barnham <joelnbarnham@gmail.com>
2022-04-10 12:02:35 +0100

    Add renovate

---
+++ .github/renovate-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  branchPrefix: 'renovate/',
+  dryRun: false,
+  username: 'renovate-bot',
+  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+  onboarding: true,
+  platform: 'github',
+  includeForks: false,
+  repositories: [
+    'joelnb/xenstore-go',
+  ],
+};
