ba46b33128db10bab217c9bd952fb73a697ae8b8
Joel Noyce Barnham <joelnbarnham@gmail.com>
2022-04-15 12:38:24 +0100

    Fix indentation

--- cmd/xenstore/log.go
+++ cmd/xenstore/log.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-    "github.com/onrik/logrus/filename"
-    log "github.com/sirupsen/logrus"
+	"github.com/onrik/logrus/filename"
+	log "github.com/sirupsen/logrus"
 )
 
 func init() {
-    setLogFormatter()
+	setLogFormatter()
 
-    filenameHook := filename.NewHook()
-    filenameHook.Field = "source"
-    filenameHook.SkipPrefixes = append(filenameHook.SkipPrefixes, "cmd/xenstore")
-    log.AddHook(filenameHook)
+	filenameHook := filename.NewHook()
+	filenameHook.Field = "source"
+	filenameHook.SkipPrefixes = append(filenameHook.SkipPrefixes, "cmd/xenstore")
+	log.AddHook(filenameHook)
 }
